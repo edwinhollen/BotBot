fb9cbf074dc44d9d4902912596ae5544addee32b
Edwin Hollen <edwinhollen@gmail.com>
2014-12-24 10:07:52 -0500

    Added hyperlink for Twitch OAuth token.

--- src/Controller.java
+++ src/Controller.java
@@ -1,8 +1,15 @@
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -11,6 +18,9 @@ public class Controller implements Initializable{
     ListView listStatusLog;
     @FXML
     Button btnConnect;
+    @FXML
+    Label lblGetTwitchOAuthToken;
+
 
     private ListViewLogger logger;
 
@@ -18,6 +28,19 @@ public class Controller implements Initializable{
     public void initialize(URL location, ResourceBundle resources) {
         logger = new ListViewLogger(listStatusLog);
 
-        logger.log("Hello world");
+        lblGetTwitchOAuthToken.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                SwingUtilities.invokeLater(() -> {
+                    try {
+                        Desktop.getDesktop().browse(new URL("http://www.twitchapps.com/tmi/").toURI());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (URISyntaxException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        });
     }
 }
